6ed2e025df15844554bd1f7de000b9a3e789d7a9
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-09 10:03:58 +0200

    initialise back with express and mongoose

---
+++ models/dbconnect.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+require('dotenv').config()
+
+var uri = process.env.DB_INFO
+
+var options = {
+    connectTimeoutMS: 5000,
+    useNewUrlParser: true
+  }
+
+mongoose.connect(uri, options, function(error){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("***** connection ok *****");
+      }
+    }
+  )
