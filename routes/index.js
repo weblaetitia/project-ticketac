9e61b32ba7efacab12870c64f45088ff3f246d22
Celine <lafont.celine@yahoo.fr>
2020-07-10 11:52:17 +0200

    route signin

--- routes/index.js
+++ routes/index.js
@@ -38,24 +38,7 @@ router.get('/notrain', function(req, res, next) {
   res.render('notrain');
 });
 
-/* Route Post Sign In */ 
-// router.post('/signin',  async function(req,res,next){
-  
-//   var searchUser = await UserModel.findOne({
-//     email: req.body.email
-//     password: req.body.password
-//   })
-  // if (!searchUser){ 
-  //   var newUser = new UserModel ({
-  
-  //     firstname: req.body.firstname,
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   })
 
-  //   res.render('search');
-  // });
   
 /* Route Sign-up*/
 router.post('/signup', async function(req,res,next){
@@ -89,6 +72,23 @@ res.render('index');
 }
 });
 
+ /* Route Post Sign In */ 
+router.post('/signin',  async function(req,res,next){
+
+  var searchUser = await UserModel.findOne({
+    email: req.body.email,
+    password: req.body.password
+    })
+    
+   if(searchUser!= null){
+    req.session.user = {
+      name : searchUser.name,
+      id: searchUser._id,
+    }
+    res.redirect('/search')
+  } else {
+     res.render('index')
+   }
+  })
 
 module.exports = router;
-
