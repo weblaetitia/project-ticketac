d9f59b6220aee236107a8b49d37ecccabbe95a0b
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-11 13:56:38 +0200

    double check if all fields are complete in signup route

--- routes/index.js
+++ routes/index.js
@@ -42,35 +42,36 @@ router.get('/notrain', function(req, res, next) {
   
 /* Route Sign-up*/
 router.post('/signup', async function(req,res,next){
-
   // verif que tous les champs sont remplis
+  if (req.body.firstname && req.body.name && req.body.email && req.body.password) {
+    // ok all inputs are completed
+    var searchUser = await UserModel.findOne({
+      email: req.body.email
+    })
+    if (!searchUser){ 
+      // if user doesn't exist
+      var newUser = new UserModel ({
+        firstname: req.body.firstname,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+    var newUserSave = await newUser.save();
+    req.session.user = {
+      name : newUserSave.name,
+      id: newUserSave._id,
+    }
+    res.redirect('/search');
+    } else { 
+      // user exist
+      res.render('index', {error2: 'User allready exists '});
+    }
+  } else {
+    // Wrong all inputs are not completed
+    res.render('index', {error2: 'All fields must be completed'});
+  }
 
-  var searchUser = await UserModel.findOne({
-    email: req.body.email
-  })
-
-if (!searchUser){ 
-  var newUser = new UserModel ({
-
-    firstname: req.body.firstname,
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-var newUserSave = await newUser.save();
-
-// console.log(newUserSave);
-// console.log(req.session)
-
-req.session.user = {
-  name : newUserSave.name,
-  id: newUserSave._id,
-}
-res.redirect('/search');
-} else { 
-  res.render('index', {error2: 'User\'s allready exists '});
-}
-});
+})
 
  /* Route Post Sign In */ 
 router.post('/signin',  async function(req,res,next){
